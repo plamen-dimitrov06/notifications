9ad75ff169f93b93fc461c8be7a8c20407954845
Plamen Dimitrov <plamen.dimitrov006@gmail.com>
2025-01-13 22:20:22 +0200

    add http handlers for the different transports

--- internal/notifications/controller.go
+++ internal/notifications/controller.go
@@ -1,1 +1,17 @@
-/* http actions for accepting messages */
+package notifications
+
+import (
+	"net/http"
+)
+
+func SlackHandler(w http.ResponseWriter, r *http.Request) {
+
+}
+
+func SMSHandler(w http.ResponseWriter, r *http.Request) {
+
+}
+
+func EmailHandler(w http.ResponseWriter, r *http.Request) {
+
+}
