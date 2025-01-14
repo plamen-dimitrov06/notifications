8cc8ff1f7b019d1eec689c2dad7b7c1ff36a9353
Plamen Dimitrov <plamen.dimitrov006@gmail.com>
2025-01-14 21:19:22 +0200

    remove attachment

--- internal/notifications/slack.go
+++ internal/notifications/slack.go
@@ -11,25 +11,11 @@ type SlackTransport struct {
 
 func (t SlackTransport) Send(m Message) {
 	api := slack.New("<API token here>")
-	attachment := slack.Attachment{
-		Pretext: "some pretext",
-		Text:    m.Content,
-		// Uncomment the following part to send a field too
-		/*
-			Fields: []slack.AttachmentField{
-				slack.AttachmentField{
-					Title: "a",
-					Value: "no",
-				},
-			},
-		*/
-	}
 
 	// TODO : move the hardcoded values to constant (channel and API key)
 	channelID, timestamp, err := api.PostMessage(
-		"<channel ID here>",
-		slack.MsgOptionText("Some text", false),
-		slack.MsgOptionAttachments(attachment),
+		"channel ID here",
+		slack.MsgOptionText(m.Content, false),
 		slack.MsgOptionAsUser(true), // Add this if you want that the bot would post message as a user, otherwise it will send response using the default slackbot
 	)
 	if err != nil {
